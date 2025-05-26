994f40c4877bbb222e2bcd7469cbf452591c8697
Odoxical <owewou02@kingshigh.school.nz>
2025-05-26 14:58:36 +1200

    Main Menu Done

--- Main_Menu.gd
+++ Main_Menu.gd
@@ -1,10 +1,10 @@
 extends Node
 
-func _on_Start_button_pressed():
-	get_tree().change_scene_to_file("res://Main_Game.tcn")
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://Settings_Menu.tscn")
 
-func _on_Settings_button_pressed():
-	get_tree().change_scene_to_file("res://Settings_Menu.tcn")
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Main_Game.tscn")
 
-func _on_exit_button_pressed():
-	get_tree().quit()
+func _on_exit_pressed() -> void:
+	get_tree().exit()
--- settings_menu.gd
+++ settings_menu.gd
@@ -1,6 +1,23 @@
 extends Node2D
 
-func _on_MainMenu_button_pressed():
-	get_tree().change_scene_to_file("res://Main_Menu.tcn")
-func _on_SliderVisibility_button_pressed():
-	get_tree().Buttonsize.visible= false
+var visibility = 0
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Main_Menu.tscn")
+
+#Visibility toggle
+func _on_slider_visibility_pressed() -> void:
+	if $Buttonsize.visible == false:
+		$Buttonsize.visible = true
+	else:
+		$Buttonsize.visible = false
+	if $Buttonsize/TextEdit.visible == false:
+		$Buttonsize/TextEdit.visible = true
+	else:
+		$Buttonsize/TextEdit.visible = false
+
+func _on_buttonsize_drag_ended(value_changed: bool) -> void:
+	$SliderVisibility.scale = Vector2($Buttonsize.value,$Buttonsize.value)
+
+func _on_font_size_slider_drag_started() -> void:
+	$"Main text".Vector2($"Font Size Slider".value,0)
