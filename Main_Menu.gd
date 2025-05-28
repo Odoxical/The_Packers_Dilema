3f84a8c1f173f5dce602841608beab2284178948
Odoxical <owewou02@kingshigh.school.nz>
2025-05-29 11:01:01 +1200

    null

--- Main_Menu.gd
+++ Main_Menu.gd
@@ -1,10 +1,10 @@
 extends Node
 
-func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://Settings_Menu.tscn")
-
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main_Game.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().exit()
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://Settings_Menu.tscn")
