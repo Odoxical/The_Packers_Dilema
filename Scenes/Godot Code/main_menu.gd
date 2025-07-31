768bde238930435bd67d189f933402119b3237a5
Odoxical <owewou02@kingshigh.school.nz>
2025-07-31 14:23:24 +1200

    Fix Break

--- Scenes/Godot Code/main_menu.gd
+++ Scenes/Godot Code/main_menu.gd
@@ -2,10 +2,10 @@ extends Node
 
 var music = 1
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Main_Game.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Main_Game.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().exit()
 
 func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://Settings_Menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Settings_Menu.tscn")
