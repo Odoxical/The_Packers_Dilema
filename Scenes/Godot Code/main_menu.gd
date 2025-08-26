6597250307ba04f2565f0407253524b9bcae94f9
Odoxical <owewou02@kingshigh.school.nz>
2025-08-26 19:31:19 +1200

    Menu updated

--- Scenes/Godot Code/main_menu.gd
+++ Scenes/Godot Code/main_menu.gd
@@ -1,11 +1,14 @@
 extends Node
-
 var music = 1
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Main_Game.tscn")
 
-func _on_exit_pressed() -> void:
-	get_tree().exit()
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Settings_Menu.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().exit()
+
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Main_Game.tscn")
